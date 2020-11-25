ab712f69c9485e551698997fbad602b9c175959f
kirebeasley <55118075+kirebeasley@users.noreply.github.com>
2020-11-24 22:10:38 -0500

    Create customerpurchase.java

---
+++ customerpurchase.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class customerpurchase {
+    private String prodpurch;
+    private double prodprice;
+
+    public String getProdpurch() {
+        return prodpurch;
+    }
+
+    public void setProdpurch(String prodpurch) {
+        this.prodpurch = prodpurch;
+    }
+
+    public double getProdprice() {
+        return prodprice;
+    }
+
+    public void setProdprice(double prodprice) {
+        this.prodprice = prodprice;
+    }
+}
